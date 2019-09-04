5765c92916412f93f2cb4321cfd9541ea416b2e9
Andrzej Heczko <heczko@logio.cz>
2019-09-04 14:52:48 +0200

    first usable version for v6.0

--- support/OddinProvider.php
+++ support/OddinProvider.php
@@ -3,7 +3,13 @@
 namespace BigBIT\Oddin\Support\Laravel;
 
 use BigBIT\Oddin\Singletons\DIResolver;
+use BigBIT\Oddin\Utils\CacheResolver;
+use BigBIT\Oddin\Utils\ClassMapResolver;
+use Illuminate\Container\Container;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\ServiceProvider;
+use Psr\Container\ContainerInterface;
+use Psr\SimpleCache\CacheInterface;
 
 /**
  * Class OddinProvider
@@ -16,6 +22,19 @@ class OddinProvider extends ServiceProvider
      */
     public function register()
     {
+        $this->app->singleton(ClassMapResolver::class, function(Application $app) {
+            return new ClassMapResolver(
+                realpath(base_path('vendor/autoload.php'))
+            );
+        });
+
+        $this->app->singleton(CacheResolver::class, function(Application $app) {
+            return new CacheResolver(
+                $app->make(ClassMapResolver::class),
+                $app->make(CacheInterface::class)
+            );
+        });
+
         DIResolver::create($this->app);
     }
 }
